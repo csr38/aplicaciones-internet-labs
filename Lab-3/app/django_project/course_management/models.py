dc85ed6a99ab50ca5084617e499f0c9215a7b9c8
Cèsar Tapia <cesar1tapia1@gmail.com>
2023-09-14 01:06:34 -0300

    Crear e importar modelo

--- Lab-3/app/django_project/course_management/models.py
+++ Lab-3/app/django_project/course_management/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Asignatura(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido_paterno = models.CharField(max_length=100)
+    apellido_materno = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    asignaturas = models.ManyToManyField(Asignatura, related_name='alumnos')
+
+    def __str__(self):
+        return self.nombre
