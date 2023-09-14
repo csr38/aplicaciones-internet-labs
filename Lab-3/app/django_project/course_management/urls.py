51b037a523350420adb6139de466941a2bee624f
Cèsar Tapia <cesar1tapia1@gmail.com>
2023-09-14 01:07:36 -0300

    Cargar y mostrar datos en formulario desde sqlite3

--- Lab-3/app/django_project/course_management/urls.py
+++ Lab-3/app/django_project/course_management/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    url('asignatura', views.asginatura, name='asignatura'),
-    url('alumno', views.alumno, name='alumno')
+    path('asignatura', views.asginatura, name='asignatura'),
+    path('alumno', views.alumno, name='alumno'),
 ]
--- Lab-3/app/django_project/course_management/views.py
+++ Lab-3/app/django_project/course_management/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from .models import Asignatura
+from .models import Alumno
+from .forms import AsignaturaForm 
+from .forms import AlumnoForm
+
 
 def index(request):
-	return render(request, 'index.html')
+    asignaturas = Asignatura.objects.all()
+    return render(request, 'index.html', {'asignaturas': asignaturas})
 
 def asginatura(request):
-	return render(request, 'asignatura.html')
+    if request.method == 'POST':
+        form = AsignaturaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = AsignaturaForm()
+
+    asignaturas = Asignatura.objects.all()
+    return render(request, 'asignatura.html', {'asignaturas': asignaturas, 'form': form})
 
 def alumno(request):
-	return render(request, 'alumno.html')
+
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = AlumnoForm()
+
+    alumnos = Alumno.objects.all()
+    return render(request, 'alumno.html', {'alumnos': alumnos, 'form': form})
