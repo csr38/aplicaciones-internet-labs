0d77b0ebcde741ca932d862296ff3743744e30fd
Cèsar Tapia <cesar1tapia1@gmail.com>
2023-09-28 10:50:57 -0300

    Coneccion api

--- Lab-4/app/django_project/course_management/views.py
+++ Lab-4/app/django_project/course_management/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .models import Course, Student
+from django.core.paginator import Paginator
+import requests
+
+import requests
 
 def index(request):
-    courses = Course.objects.all()
-    return render(request, 'index.html',{'courses':courses})
+    meme = []
+    for x in range(1, 10):
+        x+=1
+        nene = "https://api.themoviedb.org/3/trending/movie/day?api_key=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGQ4MTM5NzZmMGMxNmFiYzIyNTU4MGJmODJjZTk5MiIsInN1YiI6IjYwMDRmYzFjOTA0ZjZkMDAzZWM1NGRmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r9MO5CQMbjQBpvHwkzGq78hKBd3Z_5M_HuV3k5dOjuw&page=" + str(x)
+        nano = requests.get(nene).json()
+        nini = nano['results']
+        meme.extend(nini)
+    paginator = Paginator(meme, 16)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
+    return render(request, 'index.html', context)
