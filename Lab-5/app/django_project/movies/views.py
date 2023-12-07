558ed684c5dc0bb82881eaba06d06fdf2b7ff4ad
csr38 <cesar1tapia1@gmail.com>
2023-12-07 01:54:49 -0300

    Entrega final

--- Lab-5/app/django_project/movies/views.py
+++ Lab-5/app/django_project/movies/views.py
@@ -1,40 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import requests
-
+from .services import generate_request
 
 def index(request):
-    query = request.GET.get('q')
-    results = []
-
-
-    if query:
-        url = "https://apis.digital.gob.cl/fl/feriados/"+query
-
-        headers = {
-            "accept": "application/json",
-            "Accept-Encoding":	"gzip, deflate, br",
-            "Accept-Language":	"es-CL,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Connection":"keep-alive",
-            "Host": "apis.digital.gob.cl",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode":"navigate",
-            "Sec-Fetch-Site":"none",
-            "Sec-Fetch-User":"?1",
-            "Upgrade-Insecure-Requests":	"1",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0"
-        }
-    
-
-        data = requests.get(url, headers=headers)
-
-        print(data.json())
-
-        for feriado in data.json():
-            print(feriado["nombre"])
-            results.append({"nombre": feriado["nombre"], "fecha":feriado['fecha'], "tipo":feriado['tipo']})
-    else:
-        return HttpResponse("No query provided")
-    
-    return render(request, 'index.html', {"results":results})
-
+    return render(request, 'index.html',{'generos':generate_request()}) #Se manda a establecer la conexion con la pagina,
+                                                                        #si esta retorna un codigo 200 (OK), significa que se establecio
+                                                                        #la conexion correctamente, luego mandamos los generos
+                                                                        #como parametro a index.html
